4d2fe296c6c8b6a7f1c6aa964989434435f16b00
Kjartan Thor Wikfeldt <wikfeldt@users.noreply.github.com>
2023-04-27 12:28:06 +0200

    add plausible integration

--- content/conf.py
+++ content/conf.py
@@ -124,3 +124,10 @@ def setup(app):
     for obj in DIRECTIVES:
         app.add_directive(obj.get_cssname(), obj)
     app.add_css_file("overrides.css")
+
+import os
+if os.environ.get('GITHUB_REF', '') == 'refs/heads/main':
+    html_js_files = [
+        ('https://plausible.io/js/script.js', {"data-domain": "enccs.github.io/cmake-workshop", "defer": "\
+defer"}),
+    ]    
